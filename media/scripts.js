81c72edfaed57b5559864e70b503639204ff69a3
Kimmo Puputti <firstname.lastname@tkk.fi>
2009-11-13 20:02:13 +0200

    Added logger and map closure basic structure.

--- media/scripts.js
+++ media/scripts.js
@@ -1,9 +1,33 @@
 /*jslint white: true, onevar: true, browser: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 /*global google:false, $:false, jQuery:false*/
 
+// Simple logger to Firebug's console.
+// Wrapped with try/catch to avoid errors in other browsers.
+var LOG = function (msg) {
+    try {
+        console.log(msg);
+    } catch (e) {}
+};
+
 // Namespace object for all Javascript code.
 var GEO = {};
 
+GEO.map = (function () {
+
+    var map,
+        lat,
+        lng;
+
+    return {
+        init: function () {
+
+        }
+    };
+
+}());
+
 $(document).ready(function () {
+
+    GEO.map.init();
 
 });
