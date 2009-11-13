d9e34794105e49e0701ede05089f5a6fa9f634ad
Kimmo Puputti <firstname.lastname@tkk.fi>
2009-11-13 22:45:25 +0200

    Added admin to installed apps.

--- settings.py
+++ settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
     'django.contrib.admindocs',
     'geobrowser',
 )
