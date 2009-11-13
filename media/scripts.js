b80258e9284d779ad6757c00f810e003f2cddf87
Kimmo Puputti <firstname.lastname@tkk.fi>
2009-11-13 19:40:54 +0200

    Added jslint options and root ns.

--- media/scripts.js
+++ media/scripts.js
@@ -1,1 +1,9 @@
+/*jslint white: true, onevar: true, browser: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 /*global google:false, $:false, jQuery:false*/
+
+// Namespace object for all Javascript code.
+var GEO = {};
+
+$(document).ready(function () {
+
+});
